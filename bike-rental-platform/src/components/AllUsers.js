e8a50820f23fc15ad439775b7af385d51cba0d1c
Prashant Kumar <2018187@iiitdmj.ac.in>
2025-08-10 20:51:32 +0530

    table of all users

--- bike-rental-platform/src/components/AllUsers.js
+++ bike-rental-platform/src/components/AllUsers.js
@@ -1,29 +1,62 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { all_users_url } from "../api-urls";
 import axios from "axios";
+import { Table } from "reactstrap";
 
 function Allusers() {
-    useEffect(()=>{
-        document.title = "All Users";
-        getallUsers();
-        console.log(all_users_url)
-    },[]);
+  const [users, setUsers] = useState([]);
 
-    const getallUsers = () =>{
-        axios.get(all_users_url).then(
-            (response)=>{
-                console.log(response);
-            },
-            (error)=>{
-                console.log(error)
-            }
-        )
-    };
+  useEffect(() => {
+    document.title = "All Users";
+    getAllUsers();
+  }, []);
+
+  const getAllUsers = () => {
+    axios
+      .get(all_users_url)
+      .then((response) => {
+        console.log(response);
+        setUsers(response.data.responseData || []);
+      })
+      .catch((error) => {
+        console.error("Error fetching users:", error);
+      });
+  };
 
   return (
     <div>
       <h3>All Users</h3>
       <h6>List of all Registered Users in Application</h6>
+      <Table striped>
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>Name</th>
+            <th>Phone</th>
+            <th>Email</th>
+            <th>Active</th>
+          </tr>
+        </thead>
+        <tbody>
+          {users.length > 0 ? (
+            users.map((user, index) => (
+              <tr key={index}>
+                <th scope="row">{user.id}</th>
+                <td>{user.name}</td>
+                <td>{user.phone}</td>
+                <td>{user.email}</td>
+                <td>{user.isActive ? "Active" : "Inactive"}</td>
+              </tr>
+            ))
+          ) : (
+            <tr>
+              <td colSpan="4" className="text-center">
+                No users found
+              </td>
+            </tr>
+          )}
+        </tbody>
+      </Table>
     </div>
   );
 }
